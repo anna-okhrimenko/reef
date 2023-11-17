6ddad7ef04c3865ba572b38bf5c8d6e7cc03e5e7
anna-okhrimenko <125506103+anna-okhrimenko@users.noreply.github.com>
2023-11-17 19:30:53 +0200

    fix header? html first screen

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,5 @@
-  
+var headerHeight = document.getElementById('header-height').clientHeight;
+const firstScreenBlock = document.querySelector('.first-screen_info-block');
+
+firstScreenBlock.style.paddingTop = (headerHeight + 60)+'px';
+
